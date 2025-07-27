863f08b54767c978803ab7fb62616dcda3439ad4
KdAndrade <kauand.andrade17@gmail.com>
2025-07-26 23:52:18 -0300

    Update CoinSyncApplicationTests.java

--- coinsync/coinsync/src/test/java/com/github/KdAndrade/coinsync/CoinSyncApplicationTests.java
+++ coinsync/coinsync/src/test/java/com/github/KdAndrade/coinsync/CoinSyncApplicationTests.java
@@ -1,13 +1,31 @@
 package com.github.KdAndrade.coinsync;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Scanner;
 
-@SpringBootTest
-class CoinSyncApplicationTests {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-	@Test
-	void contextLoads() {
-	}
+import com.github.KdAndrade.coinsync.CadastroDeUsuario.Cadastro;
 
+@SpringBootApplication
+public class CoinSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinSyncApplication.class, args);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        Cadastro cadastro = new Cadastro(nome, email);
+
+        System.out.println("Usuário cadastrado com sucesso!");
+        System.out.println("Nome: " + cadastro.getNome());
+        System.out.println("Email: " + cadastro.getEmail());
+
+        sc.close();
+    }
 }
