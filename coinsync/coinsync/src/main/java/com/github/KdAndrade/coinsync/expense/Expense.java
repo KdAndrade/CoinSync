9c09b3fe206f1eb8edd9b6cda3e8f8116e30183d
KdAndrade <kauand.andrade17@gmail.com>
2025-07-29 22:05:39 -0300

    Update Expense.java

--- coinsync/coinsync/src/main/java/com/github/KdAndrade/coinsync/expense/Expense.java
+++ coinsync/coinsync/src/main/java/com/github/KdAndrade/coinsync/expense/Expense.java
@@ -1,5 +1,28 @@
-package com.github.KdAndrade.coinsync.expense;
+package com.example.coinsync.controller;
 
-public class Expense {
+import com.example.coinsync.model.Expense;
+import com.example.coinsync.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @GetMapping
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Expense createExpense(@RequestBody Expense expense) {
+        return expenseRepository.save(expense);
+    }
 }
